7671a65643d703c6f840cd429b7de4be32c17451
YuliaYuzhakova <JuliaU33@mail.ru>
2022-04-26 13:45:30 +0700

    Changed src

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int startAmount = 100; //Сумма на счете
-        int topUp = 500; //Сумма пополнения счета
+        int topUp = 1000; //Сумма пополнения счета
 
         if (topUp >= 1000) {
             int bonus = (topUp / 100); //Величина бонуса
             int totalAmount = (startAmount + topUp + bonus);
+            System.out.println("Ваш бонус равен:" + bonus);
             System.out.println("Сумма на вашем счете:" + totalAmount);
         } else {
             int bonus = 0; //Величина бонуса
             int totalAmount = (startAmount + topUp + bonus);
+            System.out.println("Ваш бонус равен:" + bonus);
             System.out.println("Сумма на вашем счете:" + totalAmount);
         }
     }
